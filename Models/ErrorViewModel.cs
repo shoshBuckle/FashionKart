4e3cfdec618a23f3fadccea75521aaa9946bb706
shoshBuckle <shouvik.sharma@outlook.com>
2021-09-23 12:13:16 +0530

    Initial Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FashionKart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
